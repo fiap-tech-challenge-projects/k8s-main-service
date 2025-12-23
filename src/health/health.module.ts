55143197b532df038da69d77dbc8dee7777b8216
Ana Beatriz Claro Shurman <anashurman@outlook.com>
2025-12-22 23:31:09 -0300

    Add Winston logger provider to HealthModule
    Introduces a 'LOGGER' provider using Winston, configured via LoggerFactory with environment variables for service name, environment, and log level. This enhances logging capabilities within the HealthModule.

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common'
+import { Logger as WinstonLogger } from 'winston'
+
+import { LoggerFactory } from '../config'
 
 import { HealthController } from './health.controller'
 
@@ -7,5 +10,17 @@ import { HealthController } from './health.controller'
  */
 @Module({
   controllers: [HealthController],
+  providers: [
+    {
+      provide: 'LOGGER',
+      useFactory: (): WinstonLogger => {
+        return LoggerFactory.createLogger(
+          process.env.SERVICE_NAME ?? 'k8s-main-service',
+          process.env.NODE_ENV ?? 'development',
+          process.env.LOG_LEVEL ?? 'debug',
+        )
+      },
+    },
+  ],
 })
 export class HealthModule {}
